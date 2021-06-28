d5eea06942844ae7c6fa49ed1626c996703ad870
shomain <52421384+tiankonganddahai@users.noreply.github.com>
2021-06-28 23:19:12 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '8303dc44049c4e22906661bf07c2f0f2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'DaHai's Blog',
+  domain: 'https://www.notion.so/8303dc44049c4e22906661bf07c2f0f2',
+  author: 'Dahai',
 
   // open graph metadata (optional)
   description: 'Example site description',
